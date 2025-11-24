121d6831903d7872dc3640a8df1be38b4f8e38a2
ssevilll <sevilsb@code.edu.az>
2025-11-24 22:19:46 +0400

    person class

---
+++ Classwork11.22.Task1/Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public abstract class Person
+{
+    private int _id;
+    public int Id;
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Age { get; set; }
+    public Person(string name, string surname)
+    {
+        _id++;
+        Id = _id;
+        Name = name;
+        Surname = surname;
+        Age = Age;
+    }
+
+    public virtual string ShowInfo()
+    {
+        return $"ID: {Id}, Name: {Name}, Surname: {Surname}, Age: {Age}";
+    }
+
+}
---
+++ Classwork11.22.Task1/Model/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Student : Person
+{
+	public decimal Point { get; set; }
+
+    public Student(string name, string surname, decimal point) : base(name, surname)
+    {
+        Point = point;
+    }
+    public override string ShowInfo()
+    {
+        return base.ShowInfo() + $", Point: {Point}";
+    }
+
+    public static bool operator < (Student student1, Student student2)
+    {
+        return student1.Point < student2.Point;
+    }
+    public static bool operator > (Student student1, Student student2)
+    {
+        return student1.Point > student2.Point;
+    }
+
+}
